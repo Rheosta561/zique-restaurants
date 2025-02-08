0f2ed79237beeeca8c66c8a2370714adf3548073
Anubhav Mishra <manubhav731@gmail.com>
2025-02-08 15:04:16 +0530

    Files added

--- backend/app.js
+++ backend/app.js
@@ -11,10 +11,7 @@ conn();
 const cors = require('cors')
 
 const mongoose = require('mongoose');
-app.use(cors({
-    origin: 'http://localhost:5173', 
-    credentials: true
-}));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(express.static(path.join(__dirname , 'public')));
